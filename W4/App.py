5f2432c0420c930a717084a0055d9504efc344bf
Md. Rayhanur Rahman <rayhan1.618@gmail.com>
2018-09-14 19:47:31 -0400

    implemented lazy loader

--- W4/App.py
+++ W4/App.py
@@ -1,18 +1,25 @@
-import Config, Sample, math, random, Num, Sym, Rows
+import Config, Sample, math, random, Num, Sym, Rows, csv
 
 random.seed(0)
 
-print('---statistics of weather---')
-csv = Rows.CSVDataImporter("weather.csv")
-csv.showStatistics()
-print('---###---\n')
+print('---statistics of weather Load All into RAM---')
+csv_reader = Rows.CSVDataImporterAtOnce("weather.csv")
 
-print('---statistics of weather long---')
-csv = Rows.CSVDataImporter("weatherLong.csv")
-csv.showStatistics()
-print('---###---\n')
+print('---statistics of weather long Load All into RAM---')
+csv_reader = Rows.CSVDataImporterAtOnce("weatherLong.csv")
 
-print('---statistics of auto---')
-csv = Rows.CSVDataImporter("auto.csv")
-csv.showStatistics()
-print('---###---\n')
+print('---statistics of auto Load All into RAM---')
+csv_reader = Rows.CSVDataImporterAtOnce("auto.csv")
+
+
+print('---statistics of weather Load Sequentially from Secondary Memory---')
+csv_reader = Rows.CSVDataImporterSequential("weather.csv")
+
+print('---statistics of weather long Load Sequentially from Secondary Memory---')
+csv_reader = Rows.CSVDataImporterSequential("weatherLong.csv")
+
+print('---statistics of auto Load Sequentially from Secondary Memory---')
+csv_reader = Rows.CSVDataImporterSequential("auto.csv")
+
+
+
--- W4/Rows.py
+++ W4/Rows.py
@@ -1,12 +1,13 @@
 import csv,sys, Num, Sym
 
-class CSVDataImporter:
+class CSVDataImporterAtOnce:
+
     def __init__(self, csvfile):
         self.nums = []
         self.syms = []
-
         with open(csvfile) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
+            # yield next(csv_reader)
             line_count = 1
             toBeIgnored = []
             toBeParsedToInt = []
@@ -40,7 +41,70 @@ class CSVDataImporter:
                                     num.increment(float(item))
 
                 line_count += 1
-        return
+        self.showStatistics()
+
+    def showStatistics(self):
+        print("(title, total, mode, frequency)")
+        for item in self.syms:
+            print(f'({item.title}, {item.total}, {item.mode}, {item.most})')
+
+        print("")
+
+        print("(title, total, mean, standard deviation)")
+        for item in self.nums:
+            print(f'({item.title}, {item.count}, {item.mean : 0.2f}, {item.sd : 0.2f})')
+        print('---###---')
+
+class CSVDataImporterSequential:
+    def __init__(self, csvfile):
+        self.nums = []
+        self.syms = []
+        self.csvfile = csvfile
+        self.csvRowsGenerator()
+        self.readRowsLineByLine()
+        self.showStatistics()
+
+    def csvRowsGenerator(self):
+        with open(self.csvfile) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                yield row
+
+    def readRowsLineByLine(self):
+            # yield next(csv_reader)
+            line_count = 1
+            toBeIgnored = []
+            toBeParsedToInt = []
+            titles = []
+            for row in self.csvRowsGenerator():
+                for index in range(0, len(row)):
+                    item = row[index].strip()
+                    titles.append(item)
+                    if line_count == 1:
+                        if '?' in item:
+                            toBeIgnored.append(index)
+                        if '$' in item:
+                            toBeParsedToInt.append(index)
+                        if '>' in item:
+                            toBeParsedToInt.append(index)
+                        if '<' in item:
+                            toBeParsedToInt.append(index)
+                    if index not in toBeIgnored:
+                        if line_count == 1:
+                            if index not in toBeParsedToInt:
+                                self.syms.append(Sym.Sym(item))
+                            if index in toBeParsedToInt:
+                                self.nums.append(Num.Num(item))
+                        else:
+                            if index not in toBeParsedToInt:
+                                sym = next((x for x in self.syms if x.title == titles[index]), None)
+                                sym.increment(item)
+                            if index in toBeParsedToInt:
+                                num = next((x for x in self.nums if x.title == titles[index]), None)
+                                if '?' not in item:
+                                    num.increment(float(item))
+
+                line_count += 1
 
     def showStatistics(self):
         print("(title, total, mode, frequency)")
@@ -53,3 +117,4 @@ class CSVDataImporter:
         for item in self.nums:
             print(f'({item.title}, {item.count}, {item.mean : 0.2f}, {item.sd : 0.2f})')
 
+        print('---***---')
